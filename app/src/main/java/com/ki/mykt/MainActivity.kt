c9e025c78d3678ff896bad978cd9820970fbf36e
SHOPZONE\S55686 <Karthikeyan.Murugaiyan@shoppersstop.com>
2019-07-27 15:24:26 +0530

    final variable declaration

--- app/src/main/java/com/ki/mykt/MainActivity.kt
+++ app/src/main/java/com/ki/mykt/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val message = "Learn Kotlin"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
